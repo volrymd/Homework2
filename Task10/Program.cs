0229ce1836cc401b389e3d6d47482e367673f4d6
JaneZan <volrymd@gmail.com>
2023-05-21 18:26:11 +0300

    task10 done

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+while (n<100 || n>999)
+{
+    Console.Write("Вы ошиблись!\nВведите трехзначное число: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+Console.WriteLine(n /10 % 10);
